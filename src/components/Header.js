1ef1a24eff730082151feb3244ae9a98ae3f7e2d
Javier Romero <romeropaolini@gmail.com>
2019-01-06 19:32:42 +0100

    Change class component to function component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,16 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <header className='top'>
-          <h1>Catch
+const Header = ({ tagline }) => (
+  <header className='top'>
+    <h1>Catch
             <span className="ofThe">
-              <span className="of">of</span>
-              <span className="the">the</span>
-            </span>
-            Day</h1>
-          <h3 className="tagline">
-            <span>{this.props.tagline}</span>
-          </h3>
-        </header>
-      </div>
-    )
-  }
-}
-
+        <span className="of">of</span>
+        <span className="the">the</span>
+      </span>
+      Day</h1>
+    <h3 className="tagline">
+      <span>{tagline}</span>
+    </h3>
+  </header>
+)
 export default Header
