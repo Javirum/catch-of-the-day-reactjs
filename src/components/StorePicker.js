0af58b619c7a0421e2b87801d966ef51530bc602
Javier Romero <romeropaolini@gmail.com>
2019-01-07 23:30:58 +0100

    Add event listener to form

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,24 @@ import React from 'react'
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    event.preventDefault();
+    const storeName = this.myInput.current.value
+    this.props.history.push(`/store/${storeName}`)
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a Store:</h2>
-        <input type="text" required placeholder='Store Name' defaultValue={getFunName()} />
+        <input
+          type="text"
+          required placeholder='Store Name'
+          defaultValue={getFunName()}
+          ref={this.myInput}
+        />
         <button type='submit'>Visit Store</button>
       </form>
     )
