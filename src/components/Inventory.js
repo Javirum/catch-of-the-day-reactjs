b7e9dd5382b1accad7526df3403e3e801e528e50
Javier Romero <romeropaolini@gmail.com>
2019-01-14 21:04:59 +0100

    Pass addFish function through props

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AddFishForm from './AddFishForm'
 
 class Inventory extends React.Component {
   render() {
     return (
       <div className='inventory'>
         <h1>Inventory</h1>
+        <AddFishForm addFish={this.props.addFish} />
       </div>
     )
   }
