21b45c481173ea0e822598f2eb39f07fbe8d62b5
Javier Romero <romeropaolini@gmail.com>
2019-01-15 21:06:15 +0100

    Updating state with loadSampleMethod

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,10 @@ class App extends React.Component {
     this.setState({ fishes })
   }
 
+  loadSampleFishes = () => {
+
+  }
+
   render() {
     return (
       <div className='catch-of-the-day'>
@@ -22,7 +26,7 @@ class App extends React.Component {
           <Header tagline='Fresh Seafood Market' />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     )
   }
